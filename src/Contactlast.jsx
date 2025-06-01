40a610bc06ca0d2f51589750a1922194499bf453
smitkakadiya45 <smitkakadiya45@gmail.com>
2025-06-01 17:23:43 +0530

    intial commit

--- src/Contactlast.jsx
+++ src/Contactlast.jsx
@@ -27,12 +27,12 @@ const Contactlast = () => {
         var massage = e.current.value;
 
 
+
         axios.post("https://g-b9eg.onrender.com/inquiry", { name, email, subject, number, massage }).then(function (response) {
             if (response.data.status === "seccess") {
                 alert("massage was sent")
             }
         })
-
 
     }
 
@@ -83,7 +83,7 @@ const Contactlast = () => {
                             <button type="submit">send massage</button>
                         </div>
                         <div className="touch-inner-child">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d158858.5851966824!2d-0.2664051595240117!3d51.52852620095036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondon%2C%20UK!5e0!3m2!1sen!2sin!4v1736431992406!5m2!1sen!2sin" title="Contact Map"  width="600" height="860" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d158858.5851966824!2d-0.2664051595240117!3d51.52852620095036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondon%2C%20UK!5e0!3m2!1sen!2sin!4v1736431992406!5m2!1sen!2sin" title="Contact Map" width="600" height="860" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </div>
                     </div>
                 </div>
--- src/Specialservicepro.jsx
+++ src/Specialservicepro.jsx
@@ -10,9 +10,9 @@ const Specialservicepro = (abc) => {
         <div id="t1" className="text">
             <Link >{abc.title}</Link>
             <p>
-                We are the assistance that assessments <br />
+                We are the assistance that assessments <br/>
                 genuinely the issues with the vehicle.
-            </p>
+            </p> 
             <Link className="btn" to="/booking">book service</Link>
         </div>
     </>
